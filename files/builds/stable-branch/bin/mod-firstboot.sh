b99b3f93f217fa636a453117ec17c60e70c89f3e
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2023-09-17 12:24:44 +0200

    Create mod-firstboot.sh

---
+++ files/builds/stable-branch/bin/mod-firstboot.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+sleep 10
+(
+echo "# Waiting for Internet connection"
+until /usr/bin/ping -q -c 1 8.8.8.8; do sleep 1; done
+echo "10"
+
+echo "# Adding Flathub Repository"
+/usr/bin/flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+if [ "$?" != 0 ] ; then
+        zenity --error \
+          --text="Adding Flathub Repo Failed"
+        exit 1
+fi
+echo "20"
+
+echo "# Installing Firefox"
+/usr/bin/flatpak install --user --noninteractive flathub org.mozilla.firefox
+if [ "$?" != 0 ] ; then
+        zenity --error \
+          --text="Installing Firefox Failed"
+        exit 1
+fi
+
+echo "40"
+echo "# Installing Calculator"
+/usr/bin/flatpak install --user --noninteractive flathub org.gnome.Calculator
+if [ "$?" != 0 ] ; then
+        zenity --error \
+          --text="Installing Calculator Failed"
+        exit 1
+fi
+
+echo "60"
+echo "# Installing Text Editor"
+/usr/bin/flatpak install --user --noninteractive flathub org.xfce.mousepad
+if [ "$?" != 0 ] ; then
+        zenity --error \
+          --text="Installing Text Editor Failed"
+        exit 1
+fi
+
+echo "100"
+echo "# Cleaning up"
+
+rm ~/.config/autostart/mod-firstboot.desktop
+) |
+        zenity --progress --title="MicroOS Desktop Firstboot" --percentage=0 --auto-close --no-cancel --width=300
+
+if [ "$?" != 0 ] ; then
+        zenity --error \
+          --title="MicroOS Desktop Firstboot" --text="Firstboot configuration was not completed correctly!"
+else
+        zenity --info \
+          --title="MicroOS Desktop Firstboot" --text="Firstboot configuration was completed successfully!"
+fi
+rm -f /home/steve/.config/autostart/mod-firstboot.desktop
+rm -f /home/steve/.config/autostart/mod-firstboot.sh
