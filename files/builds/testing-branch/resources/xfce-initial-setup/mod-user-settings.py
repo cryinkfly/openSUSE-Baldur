82562070f8175bab84b1d124501e860ba8475e2e
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2023-12-16 20:59:53 +0000

    Update mod-user-settings.py

--- files/builds/testing-branch/resources/xfce-initial-setup/mod-user-settings.py
+++ files/builds/testing-branch/resources/xfce-initial-setup/mod-user-settings.py
@@ -5,10 +5,72 @@ import os
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
-class RadioListApp(Gtk.Window):
+
+class Window1(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self, title="Users Settings Manager - Add User")
+        self.set_default_size(500, 350)
+        self.set_position(Gtk.WindowPosition.CENTER)
+
+        # Add widgets to Window1
+        self.label = Gtk.Label("This is Window 1 where you can add a new user.")
+        self.add(self.label)
+
+class Window2(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self, title="Window 2")
+        self.set_default_size(500, 350)
+        self.set_position(Gtk.WindowPosition.CENTER)
+
+        self.set_border_width(10)
+
+        # Vertical box to hold all widgets
+        vbox = Gtk.VBox(spacing=10)
+        self.add(vbox)
+
+        # Username entry
+        self.username_entry = Gtk.Entry()
+        self.username_entry.set_placeholder_text("Username")
+        vbox.pack_start(self.username_entry, True, True, 0)
+
+        # Password entry
+        self.password_entry = Gtk.Entry()
+        self.password_entry.set_visibility(False)
+        self.password_entry.set_placeholder_text("Password")
+        vbox.pack_start(self.password_entry, True, True, 0)
+
+        # Password confirmation entry
+        self.confirm_password_entry = Gtk.Entry()
+        self.confirm_password_entry.set_visibility(False)
+        self.confirm_password_entry.set_placeholder_text("Confirm Password")
+        vbox.pack_start(self.confirm_password_entry, True, True, 0)
+
+        # Checkbox
+        self.checkbox = Gtk.CheckButton("I agree to the terms and conditions")
+        vbox.pack_start(self.checkbox, True, True, 0)
+
+        # Submit button
+        submit_button = Gtk.Button(label="Submit")
+        submit_button.connect("clicked", self.on_submit_clicked)
+        vbox.pack_start(submit_button, True, True, 0)
+
+    def on_submit_clicked(self, widget):
+        username = self.username_entry.get_text()
+        password = self.password_entry.get_text()
+        confirm_password = self.confirm_password_entry.get_text()
+        agreement = self.checkbox.get_active()
+
+        if password == confirm_password and agreement:
+            print(f"Username: {username}")
+            print(f"Password: {password}")
+            print("Registration successful!")
+        else:
+            print("Registration failed. Please check your inputs.")
+
+class MainWindow(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="Users Settings Manager")
-        self.set_default_size(500, 0)
+        self.set_default_size(500, 350)
         self.set_position(Gtk.WindowPosition.CENTER)
 
         check_file="""
@@ -62,6 +124,17 @@ class RadioListApp(Gtk.Window):
         """
         os.system(check_file)
 
+        # Add a Description
+        label = Gtk.Label()
+        label.set_markup(
+            "Text can be <small>small</small>, <big>big</big>, "
+            "<b>bold</b>, <i>italic</i> and even point to "
+            'somewhere in the <a href="https://www.gtk.org" '
+            'title="Click to find out more">internets</a>.'
+        )
+        label.set_line_wrap(True)
+        label.set_max_width_chars(48)
+
         # Load options from a text file
         options = self.load_options_from_file("/tmp/_all_users_list_.XXXXXXX")
 
@@ -96,14 +169,17 @@ class RadioListApp(Gtk.Window):
         extra_button_1.connect("clicked", self.on_del_clicked)
 
         # Button Box
-        button_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=6)
+        button_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=5)
         button_box.pack_start(close_button, True, True, 0)
         button_box.pack_start(add_button, True, True, 0)
         button_box.pack_start(extra_button_0, True, True, 0)
         button_box.pack_start(extra_button_1, True, True, 0)
 
         # Main Box
-        main_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+        main_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=5)
+        # Add widgets to Window1
+
+        main_box.pack_start(label, False, True, 10)
         main_box.pack_start(treeview, True, True, 0)
         main_box.pack_start(button_box, False, False, 0)
         main_box.set_border_width(10)
@@ -125,10 +201,16 @@ class RadioListApp(Gtk.Window):
 
         Gtk.main_quit()
 
-    def on_configure_clicked(self, button):
+    def on_configure_clicked(self, widget):
         selected_option = self.get_selected_option()
         if selected_option:
             print(f"Configure button clicked. Selected option: {selected_option}")
+            
+            window1 = Window1()
+            window1.connect("destroy", Gtk.main_quit)
+            window1.show_all()
+        else:
+            print(f"Click Configure button was triggered but no option was selected!")
     
     def on_add_clicked(self, button):
         selected_option = self.get_selected_option()
@@ -148,7 +230,11 @@ class RadioListApp(Gtk.Window):
             selection = self.radio_list.iter_next(selection)
         return None
 
-win = RadioListApp()
-win.connect("destroy", Gtk.main_quit)
-win.show_all()
-Gtk.main()
+def main():
+    win = MainWindow()
+    win.connect("destroy", Gtk.main_quit)
+    win.show_all()
+    Gtk.main()
+
+if __name__ == "__main__":
+    main()
