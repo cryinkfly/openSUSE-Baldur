fe81dca37962db920dca15220f4dc2e18969ec19
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2024-05-04 22:45:07 +0200

    Update minimal-install.sh

--- minimal-install.sh
+++ minimal-install.sh
@@ -270,6 +270,7 @@ else
     zenity \
     zypper \
     zypper-needs-restarting
+    
     transactional-update apply
     echo -e "${GREEN}The required packages without the graphics card driver have been successfully installed!${NOCOLOR}"
     echo -e "${YELLOW}The graphics card driver is being analyzed on your system!${NOCOLOR}"
