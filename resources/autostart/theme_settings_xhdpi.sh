d2021e8dd4d0bfc47ee9354764ccd82ee79ce0a1
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2024-05-07 19:00:18 +0200

    Create theme_settings_xhdpi.sh

---
+++ resources/autostart/theme_settings_xhdpi.sh
@@ -0,0 +1,14 @@
+#!/bin/env bash
+
+xfconf-query -c xsettings -p /Net/ThemeName -s Nordic-v40-xhdpi
+xfconf-query -c xfwm4 -p /general/theme -s Nordic-v40-xhdpi
+xfconf-query -c xsettings -p /Gdk/WindowScalingFactor -s 2
+flatpak override --filesystem=$HOME/.themes
+flatpak override --filesystem=$HOME/.icons
+flatpak override --env=GTK_THEME=Nordic-v40-xhdpi 
+flatpak override --env=ICON_THEME=Tela-circle-manjaro-dark 
+flatpak override --env=CURSOR_THEME=Bibata-Modern-Classic
+
+rm -f $HOME/.config/autostart/theme_settings_xhdpi.desktop
+# After finishing execution, delete the script itself
+rm "$0"
