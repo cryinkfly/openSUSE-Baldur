aacf39d612c927f4c2d0e5e6a25c0df88b149ece
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2024-05-08 17:49:49 +0200

    Update and rename theme_settings_hdpi.sh to mod-theme_config.sh

---
+++ resources/autostart/mod-theme_config.sh
@@ -0,0 +1,30 @@
+#!/bin/env bash
+
+X=$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f1)
+Y=$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f2)
+display_resolution="$X"x"$Y"
+
+if [ "$display_resolution" = "3840x2160" ]; then
+  xfconf-query -c xsettings -p /Net/ThemeName -s Nordic-v40-xhdpi
+  xfconf-query -c xfwm4 -p /general/theme -s Nordic-v40-xhdpi
+  xfconf-query -c xsettings -p /Gdk/WindowScalingFactor -s 2
+  flatpak override --filesystem=$HOME/.themes
+  flatpak override --filesystem=$HOME/.icons
+  flatpak override --env=GTK_THEME=Nordic-v40-xhdpi 
+  flatpak override --env=ICON_THEME=Tela-circle-manjaro-dark 
+  flatpak override --env=CURSOR_THEME=Bibata-Modern-Classic
+else
+  xfconf-query -c xsettings -p /Net/ThemeName -s Nordic-v40
+  xfconf-query -c xfwm4 -p /general/theme -s Nordic-v40
+  xfconf-query -c xsettings -p /Net/IconThemeName -s Tela-circle-manjaro-dark
+  xfconf-query -c xsettings -p /Gtk/CursorThemeName -s Bibata-Modern-Classic
+  flatpak override --filesystem=$HOME/.themes
+  flatpak override --filesystem=$HOME/.icons
+  flatpak override --env=GTK_THEME=Nordic-v40 
+  flatpak override --env=ICON_THEME=Tela-circle-manjaro-dark 
+  flatpak override --env=CURSOR_THEME=Bibata-Modern-Classic
+fi
+
+rm -f $HOME/.config/autostart/theme_settings_hdpi.desktop
+# After finishing execution, delete the script itself
+rm "$0"
