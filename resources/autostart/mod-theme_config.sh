713bdd4a32e4e4ca8d62fc08b9a64ef6b6d3cf8f
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2024-05-10 16:32:08 +0200

    Update mod-theme_config.sh

--- resources/autostart/mod-theme_config.sh
+++ resources/autostart/mod-theme_config.sh
@@ -6,7 +6,7 @@ is_4k=false
 
 # Check if any monitor is 4K
 while read -r line; do
-    if [[ $line == *4K* ]]; then
+    if [[ $line == *3840x2160* ]]; then
         is_4k=true
         break
     fi
@@ -17,21 +17,21 @@ if $is_4k; then
     xfconf-query -c xsettings -p /Net/ThemeName -s Nordic-v40-xhdpi
     xfconf-query -c xfwm4 -p /general/theme -s Nordic-v40-xhdpi
     xfconf-query -c xsettings -p /Gdk/WindowScalingFactor -s 2
-    flatpak override --filesystem=$HOME/.themes
-    flatpak override --filesystem=$HOME/.icons
-    flatpak override --env=GTK_THEME=Nordic-v40-xhdpi 
-    flatpak override --env=ICON_THEME=Tela-circle-manjaro-dark 
-    flatpak override --env=CURSOR_THEME=Bibata-Modern-Classic
+    flatpak override --user --filesystem=$HOME/.themes
+    flatpak override --user --filesystem=$HOME/.icons
+    flatpak override --user --env=GTK_THEME=Nordic-v40-xhdpi 
+    flatpak override --user --env=ICON_THEME=Tela-circle-manjaro-dark 
+    flatpak override --user --env=CURSOR_THEME=Bibata-Modern-Classic
 else
     xfconf-query -c xsettings -p /Net/ThemeName -s Nordic-v40
     xfconf-query -c xfwm4 -p /general/theme -s Nordic-v40
     xfconf-query -c xsettings -p /Net/IconThemeName -s Tela-circle-manjaro-dark
     xfconf-query -c xsettings -p /Gtk/CursorThemeName -s Bibata-Modern-Classic
-    flatpak override --filesystem=$HOME/.themes
-    flatpak override --filesystem=$HOME/.icons
-    flatpak override --env=GTK_THEME=Nordic-v40 
-    flatpak override --env=ICON_THEME=Tela-circle-manjaro-dark 
-    flatpak override --env=CURSOR_THEME=Bibata-Modern-Classic
+    flatpak override --user --filesystem=$HOME/.themes
+    flatpak override --user --filesystem=$HOME/.icons
+    flatpak override --user --env=GTK_THEME=Nordic-v40 
+    flatpak override --user --env=ICON_THEME=Tela-circle-manjaro-dark 
+    flatpak override --user --env=CURSOR_THEME=Bibata-Modern-Classic
 fi
 
 rm -f $HOME/.config/autostart/mod-theme_config.desktop
