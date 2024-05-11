4350079e3633d07a9a208718ea1cc894db40feae
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2024-05-11 07:26:30 +0200

    Update mod-theme_config.sh

--- resources/autostart/mod-theme_config.sh
+++ resources/autostart/mod-theme_config.sh
@@ -14,25 +14,35 @@ done <<< "$connected_monitors"
 
 # If 4K monitor is detected
 if $is_4k; then
-    xfconf-query -c xsettings -p /Net/ThemeName -s Nordic-v40-xhdpi
-    xfconf-query -c xfwm4 -p /general/theme -s Nordic-v40-xhdpi
-    xfconf-query -c xsettings -p /Gdk/WindowScalingFactor -s 2
+    curl https://raw.githubusercontent.com/cryinkfly/openSUSE-Baldur/main/resources/xfce4/xfconf/xfce-perchannel-xml/xfwm4-xhdpi.xml > $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
+    curl https://raw.githubusercontent.com/cryinkfly/openSUSE-Baldur/main/resources/xfce4/xfconf/xfce-perchannel-xml/xsettings-xhdpi.xml > $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml
+    #xfconf-query -c xsettings -p /Net/ThemeName -s Nordic-v40-xhdpi
+    #xfconf-query -c xfwm4 -p /general/theme -s Nordic-v40-xhdpi
+    #xfconf-query -c xsettings -p /Gdk/WindowScalingFactor -s 2
+    xfwm4 --replace
+    xfsettingsd --replace
     flatpak override --user --filesystem=$HOME/.themes
     flatpak override --user --filesystem=$HOME/.icons
     flatpak override --user --env=GTK_THEME=Nordic-v40-xhdpi 
     flatpak override --user --env=ICON_THEME=Tela-circle-manjaro-dark 
     flatpak override --user --env=CURSOR_THEME=Bibata-Modern-Classic
 else
-    xfconf-query -c xsettings -p /Net/ThemeName -s Nordic-v40
-    xfconf-query -c xfwm4 -p /general/theme -s Nordic-v40
-    xfconf-query -c xsettings -p /Net/IconThemeName -s Tela-circle-manjaro-dark
-    xfconf-query -c xsettings -p /Gtk/CursorThemeName -s Bibata-Modern-Classic
+    curl https://raw.githubusercontent.com/cryinkfly/openSUSE-Baldur/main/resources/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml > $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
+    curl https://raw.githubusercontent.com/cryinkfly/openSUSE-Baldur/main/resources/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml > $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml
+    #xfconf-query -c xsettings -p /Net/ThemeName -s Nordic-v40
+    #xfconf-query -c xfwm4 -p /general/theme -s Nordic-v40
+    #xfconf-query -c xsettings -p /Net/IconThemeName -s Tela-circle-manjaro-dark
+    #xfconf-query -c xsettings -p /Gtk/CursorThemeName -s Bibata-Modern-Classic
+    xfwm4 --replace
+    xfsettingsd --replace
     flatpak override --user --filesystem=$HOME/.themes
     flatpak override --user --filesystem=$HOME/.icons
     flatpak override --user --env=GTK_THEME=Nordic-v40 
     flatpak override --user --env=ICON_THEME=Tela-circle-manjaro-dark 
     flatpak override --user --env=CURSOR_THEME=Bibata-Modern-Classic
 fi
+
+sleep 10
 
 rm -f $HOME/.config/autostart/mod-theme_config.desktop
 # After finishing execution, delete the script itself
