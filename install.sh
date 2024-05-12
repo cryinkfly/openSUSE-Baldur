2413aa8e9da5238741ac896e29084ec4bfc93078
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2024-05-12 12:05:56 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -373,21 +373,20 @@ else
 
     # If 4K monitor is detected
     if $is_4k; then
-        curl https://raw.githubusercontent.com/cryinkfly/openSUSE-Baldur/main/resources/xfce4/xfconf/xfce-perchannel-xml/xfwm4-xhdpi.xml > ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
-        curl https://raw.githubusercontent.com/cryinkfly/openSUSE-Baldur/main/resources/xfce4/xfconf/xfce-perchannel-xml/xsettings-xhdpi.xml > ~/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml
-        flatpak override --filesystem=$HOME/.themes
-        flatpak override --filesystem=$HOME/.icons
-        flatpak override --env=GTK_THEME=Nordic-v40-xhdpi 
-        flatpak override --env=ICON_THEME=Tela-circle-manjaro-dark 
-        flatpak override --env=CURSOR_THEME=Bibata-Modern-Classic
+        xfconf-query -c xsettings -p /Net/ThemeName -s Nordic-v40-xhdpi
+        xfconf-query -c xfwm4 -p /general/theme -s Nordic-v40-xhdpi
+        sed -i 's/Gdk\/WindowScalingFactor\>/Gdk\/WindowScalingFactor>2/g' ~/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml
+        xfconf-query -c xsettings -p /Gdk/WindowScalingFactor -n -t int -s 2
+        xfconf-query -c xsettings -p /Net/IconThemeName -s Tela-circle-manjaro-dark
+        xfconf-query -c xsettings -p /Gtk/CursorThemeName -s Bibata-Modern-Classic
+        xfconf-query -c xsettings -p /Gtk/CursorThemeSize -s 38
+        xfce4-panel -r
     else
-        curl https://raw.githubusercontent.com/cryinkfly/openSUSE-Baldur/main/resources/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml > ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
-        curl https://raw.githubusercontent.com/cryinkfly/openSUSE-Baldur/main/resources/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml > ~/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml
-        flatpak override --filesystem=$HOME/.themes
-        flatpak override --filesystem=$HOME/.icons
-        flatpak override --env=GTK_THEME=Nordic-v40 
-        flatpak override --env=ICON_THEME=Tela-circle-manjaro-dark 
-        flatpak override --env=CURSOR_THEME=Bibata-Modern-Classic
+        xfconf-query -c xsettings -p /Net/ThemeName -s Nordic-v40
+        xfconf-query -c xfwm4 -p /general/theme -s Nordic-v40
+        xfconf-query -c xsettings -p /Net/IconThemeName -s Tela-circle-manjaro-dark
+        xfconf-query -c xsettings -p /Gtk/CursorThemeName -s Bibata-Modern-Classic
+        xfce4-panel -r
     fi
     echo -e "${GREEN}Ttheme, icons, wallpapers, ... has been successfully installed!${NOCOLOR}"
 
