0ee176eeefa62eac6bf81f1883c731fcc91558de
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2024-05-12 07:29:51 +0200

    Update mod-theme_config.sh

--- resources/autostart/mod-theme_config.sh
+++ resources/autostart/mod-theme_config.sh
@@ -20,7 +20,7 @@ if $is_4k; then
     #curl https://raw.githubusercontent.com/cryinkfly/openSUSE-Baldur/main/resources/xfce4/xfconf/xfce-perchannel-xml/xsettings-xhdpi.xml > $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml
     xfconf-query -c xsettings -p /Net/ThemeName -s Nordic-v40-xhdpi
     xfconf-query -c xfwm4 -p /general/theme -s Nordic-v40-xhdpi
-    xfconf-query -c xsettings --property /Gdk/WindowScalingFactor -s 2
+    sleep 5 && xfconf-query -c xsettings --property /Gdk/WindowScalingFactor -s 2
     xfconf-query -c xsettings -p /Net/IconThemeName -s Tela-circle-manjaro-dark
     xfconf-query -c xsettings -p /Gtk/CursorThemeName -s Bibata-Modern-Classic
     xfconf-query -c xsettings -p /Gtk/CursorThemeSize -s 38
